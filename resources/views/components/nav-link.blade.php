e67ac19b96d30ad6f0144c4dfded4c4b4e192595
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-10-02 07:44:28 +0700

    feat[homepage]: redesign navbar ui

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,8 +1,8 @@
 @props(['active', 'href'])
 
 @php
-    $activeClasses = 'inline-flex items-center px-1 pt-1 border-b-2 border-pink-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-pink-700 transition duration-150 ease-in-out';
-    $defaultClasses = 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+    $activeClasses = 'text-white inline-flex items-center px-1 py-1 border-b-2 border-pink-300 text-md font-medium leading-5 text-gray-900 focus:outline-none focus:border-pink-700 transition duration-150 ease-in-out';
+    $defaultClasses = 'inline-flex items-center px-1 py-1 border-b-2 border-transparent text-md font-medium leading-5 text-gray-500 hover:text-[#ffd700] hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 
     // Ambil ID dari href, contoh: dari '#products' menjadi 'products'
     $linkId = ($href === route('home')) ? 'hero' : substr($href, 1);
